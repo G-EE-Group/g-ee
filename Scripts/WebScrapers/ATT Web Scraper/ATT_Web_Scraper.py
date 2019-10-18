f773e9f14c61af64dd005e63d1de48d5dae3eb87
SolarSistim <47832728+SolarSistim@users.noreply.github.com>
2019-10-18 15:13:43 -0500

    Updated to use external config.ini file for username, password and account number.

--- Scripts/WebScrapers/ATT Web Scraper/ATT_Web_Scraper.py
+++ Scripts/WebScrapers/ATT Web Scraper/ATT_Web_Scraper.py
@@ -5,54 +5,63 @@ import time
 import requests
 
 options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
+options.add_experimental_option("excludeSwitches",["ignore-certificate-errors"])
 options.add_argument('--disable-gpu')
-#options.add_argument('--headless')
-# ***********************************************
-# Be sure to add the path to your chromedriver: *
-# ***********************************************
+# options.add_argument('--headless')
 driver = webdriver.Chrome(options=options, executable_path=r'C:\chromedriver.exe')
 
-driver.implicitly_wait(10)
+# **************************************************************
+# Extract username, password and account number from config.ini*
+# **************************************************************
 
+mylines = []
+with open ('config.ini', 'rt') as myfile:
+    for myline in myfile:
+        mylines.append(myline)
+
+ATT_Username = (mylines[0])
+ATT_Password = (mylines[1])
+ATT_Account_Number = (mylines[2])
+
+ATT_Username = ATT_Username.replace('AT&T Username = ', '')
+ATT_Username = ATT_Username.replace('\n', '')
+ATT_Username = ATT_Username.replace('\t', '')
+ATT_Password = ATT_Password.replace('AT&T Password = ', '')
+ATT_Password = ATT_Password.replace('\n', '')
+ATT_Password = ATT_Password.replace('\t', '')
+ATT_Account_Number = ATT_Account_Number.replace('AT&T Account Number = ', '')
+ATT_Account_Number = ATT_Account_Number.replace('\n', '')
+ATT_Account_Number = ATT_Account_Number.replace('\t', '')
+
+# **********************************************
+# Open AT&T web page and begin extracting data *
+# **********************************************
+
+driver.implicitly_wait(10)
 driver.get("https://www.att.com/my/")
-print("Navigated to https://www.att.com/my/")
-time.sleep(1)
 
 time.sleep(10)
 username = driver.find_element_by_id("userName")
 print("username located")
-time.sleep(1)
 password = driver.find_element_by_id("password")
 print("password located")
-# ***********************************************
-# Add your AT&T Username/Email address here:    *
-# ***********************************************
-username.send_keys("USERNAME")
+username.send_keys(ATT_Username)
 print("username entered")
 time.sleep(1)
-# ***********************************************
-# Add your AT&T password here:                  *
-# ***********************************************
-password.send_keys("PASSWORD")
+password.send_keys(ATT_Password)
 print("password entered")
 time.sleep(1)
 driver.find_element_by_id("loginButton-lgwgLoginButton").click()
 print("Login button clicked")
-time.sleep(20)
-elementAttDueDate = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/div/div[1]/p[2]/span")
-print("Located Due Date field")
-time.sleep(1)
+time.sleep(10)
+elementAttDueDate= driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/div/div[1]/p[2]/span")
 attDueDate = elementAttDueDate.text
 attDueDate = attDueDate.replace('Pay by ', '')
-# ************************************************************
-# Add your AT&T account number at the end of the line below: *
-# ************************************************************
-driver.get("https://www.att.com/olam/passthroughAction.myworld?actionType=UsageLanding&selectedBan=ATTACCOUNTNUMBER")
+print(attDueDate)
+driver.get("https://www.att.com/olam/passthroughAction.myworld?actionType=UsageLanding&selectedBan=" + ATT_Account_Number)
 print("navigated to bill page")
 time.sleep(10)
 innerHTML = driver.execute_script("return document.body.innerHTML")
-time.sleep(3)
 time.sleep(3)
 driver.find_element_by_xpath("//*[@id='divShortcut']/div[1]").click()
 time.sleep(3)
@@ -60,20 +69,16 @@ driver.find_element_by_xpath("//*[@id='RecentUnbilled']/div[2]/a").click()
 
 soup = BeautifulSoup(innerHTML, 'html.parser')
 time.sleep(1)
-elementAttAmountDue = driver.find_element_by_xpath("//*[@id='content']/div[3]/div[1]/div[2]/span")
-print("Located Amount Due field")
-time.sleep(1)
-elementAttBandwidthUsed = driver.find_element_by_xpath("//*[@id='countlargenumbers']/span/strong")
-print("Located Bandwidth Quota field")
-time.sleep(1)
+elementAttAmountDue= driver.find_element_by_xpath("//*[@id='content']/div[3]/div[1]/div[2]/span")
+elementAttBandwidthUsed= driver.find_element_by_xpath("//*[@id='countlargenumbers']/span/strong")
 attAmountDue = elementAttAmountDue.text
 attBandwidthUsed = elementAttBandwidthUsed.text
 
 attAmountDue = attAmountDue.replace('$', '')
 
-print("Amount due: " + attAmountDue)
-print("Bandwidth used: " + attBandwidthUsed + " Gb")
-print("Due date: " + attDueDate)
+print(attAmountDue)
+print(attBandwidthUsed)
+print(attDueDate)
 
 line1 = ("# DML\\n")
 line2 = ("\n")
